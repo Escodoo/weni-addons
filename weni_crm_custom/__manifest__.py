3e1976168fe45779ff623dc78872459bb8b734dc
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-05-21 18:50:37 -0300

    [ADD] weni_crm_custom

---
+++ weni_crm_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weni Crm Custom',
+    'description': """
+        Customizações do Modulo CRM - Weni""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'crm',
+        'crm_secondary_salesperson'
+    ],
+    'data': [
+        'views/crm_lead.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ weni_crm_custom/models/crm_lead.py
@@ -0,0 +1,62 @@
+# Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class CrmLead(models.Model):
+
+    _inherit = 'crm.lead'
+
+    secondary_user_id = fields.Many2one(
+        'res.users',
+        string='Pre Salesperson')
+
+    weni_company_size = fields.Integer(
+        string='Company Size')
+
+    weni_average_ticket = fields.Monetary(
+        'Average Ticket',
+        currency_field='company_currency',
+        track_visibility='always')
+
+    weni_technological_maturity = fields.Char(
+        string="Technological Maturity")
+
+    weni_has_dedicate_team = fields.Boolean(
+        string="Has Dedicated Team")
+
+    weni_active_contact = fields.Integer(
+        string='Active Contact')
+
+    weni_project_financier = fields.Many2one(
+        comodel_name='res.partner',
+        string='Project Financier')
+
+    weni_project_budget = fields.Monetary(
+        'Budget',
+        currency_field='company_currency',
+        track_visibility='always')
+
+    weni_project_time_window = fields.Date(
+        'Time Window')
+
+    weni_project_gap = fields.Html(
+        string='Gap')
+
+    weni_project_gap_primary = fields.Html(
+        string='Primary Gap')
+
+    weni_project_gap_secondary = fields.Html(
+        string='Secondary Gap')
+
+    weni_project_integration = fields.Html(
+        string='Integrations')
+
+    weni_icp_classification = fields.Selection(
+        string='ICP Classification',
+        selection=[
+            ('no_fit', 'No FIT'),
+            ('medium_fit', 'Medium FIT'),
+            ('high_fit', 'High FIT')
+        ],)
