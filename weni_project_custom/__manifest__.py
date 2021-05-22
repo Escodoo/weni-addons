bcf2fdc9a83ecddc2d800739abec0d655c6880c1
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-05-22 15:17:03 -0300

    [ADD] weni_project_custom

---
+++ weni_project_custom/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weni Project Custom',
+    'description': """
+        Weni Project Custom""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/project_project.xml',
+        'views/res_partner.xml',
+    ],
+    'demo': [
+    ],
+}
