ee07d35f86bc866a5ead21315fe340afc5313510
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-05-24 03:22:49 -0300

    [ADD] weni_contract_custom

---
+++ weni_contract_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weni Contract Custom',
+    'description': """
+        Weni Contract Custom""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'contract_forecast',
+    ],
+    'data': [
+        'security/contract_line_consumption.xml',
+        'views/contract_line_consumption.xml',
+        'data/contract_variable_qty_consumption.xml',
+    ],
+    'demo': [
+    ],
+}
