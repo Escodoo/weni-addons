5d2e644507e89c5ca5f652a1b012349e2ac0259d
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-06-18 20:00:55 -0300

    [ADD] weni_sale_custom

---
+++ weni_sale_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weni Sale Custom',
+    'description': """
+        WENI Sale Custom""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'sale',
+        'product_contract',
+    ],
+    'data': [
+        'views/sale_order.xml',
+        'views/sale_order_report_templates.xml',
+    ],
+}
