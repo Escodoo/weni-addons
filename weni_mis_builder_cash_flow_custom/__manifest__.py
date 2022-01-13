d661468afde928b9fa2aa2878b89ed9b68fc2a71
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-01-13 16:28:04 -0300

    [ADD] weni_mis_builder_cash_flow_custom

---
+++ weni_mis_builder_cash_flow_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Weni Mis Builder Cash Flow Custom',
+    'summary': """
+        Weni Mis Builder Cash Flow Custom""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+        'mis_builder_cash_flow',
+    ],
+    'data': [
+        'views/mis_cash_flow_forecast_line.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ weni_mis_builder_cash_flow_custom/models/mis_cash_flow_forecast_line.py
@@ -0,0 +1,9 @@
+# Copyright 2021 - TODAY, Marcel Savegnago
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+
+
+class MisCash_flowForecast_line(models.Model):
+
+    _inherit = "mis.cash_flow.forecast_line"
