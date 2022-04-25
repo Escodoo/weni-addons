a4bf0aca568e8c68bc753ebcede2f6a50a7426a4
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-04-25 14:01:19 -0300

    [IMP] weni_contract_consumption: add contract_line_consumption_ids field on contract.line

---
+++ weni_contract_consumption/models/contract_line.py
@@ -0,0 +1,15 @@
+# Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+
+
+class ContractLine(models.Model):
+
+    _inherit = "contract.line"
+
+    contract_line_consumption_ids = fields.One2many(
+        'contract.line.consumption',
+        'contract_line_id',
+        'Contract Consumption'
+    )
