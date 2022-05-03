18611e22dd25313c87e054a72998d505178d9ac5
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-05-03 13:05:29 -0300

    [IMP] weni_crm_custom: form refactor, lost-date and others

--- weni_crm_custom/models/crm_lead.py
+++ weni_crm_custom/models/crm_lead.py
@@ -2,44 +2,78 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
+from datetime import date
 
 
 class CrmLead(models.Model):
 
     _inherit = 'crm.lead'
 
+    # Main
     secondary_user_id = fields.Many2one(
         'res.users',
-        string='Pre Salesperson')
-    weni_company_size = fields.Integer(
-        string='Company Size')
-    weni_average_selling_price = fields.Monetary(
-        'Average Selling Price',
+        string='Pre Salesperson'
+    )
+    planned_revenue = fields.Monetary(
+        string='Monthly Recurring Revenue'
+    )
+    weni_amount_nrr = fields.Monetary(
+        'Amount NRR',
         currency_field='company_currency',
-        track_visibility='always')
-    weni_technological_maturity = fields.Selection(
-        string='Technological Maturity',
-        selection=[
-            ('low', 'Low'),
-            ('medium', 'Medium'),
-            ('high', 'High')
-        ],)
-    weni_has_dedicate_team = fields.Boolean(
-        string="Has Dedicated Team")
-    weni_active_contacts = fields.Integer(
-        string='Active Contacts')
-    weni_project_budget = fields.Monetary(
-        'Budget',
-        currency_field='company_currency',
-        track_visibility='always')
+        track_visibility='always',
+        help='Amount Non-Recurring Revenue'
+    )
     weni_project_release_date = fields.Date(
-        'Release Date')
-    weni_project_pain_primary = fields.Html(
-        string='Primary Pains')
-    weni_project_pain_secondary = fields.Html(
-        string='Secondary Pains')
-    weni_project_integration = fields.Html(
-        string='Integrations')
+        'Release Date'
+    )
+    weni_sale_type = fields.Selection(
+        string='Sale Type',
+        selection=[
+            ('platform', 'Platform'),
+            ('project', 'Project'),
+            ('enterprise', 'Enterprise Project')
+        ],)
+    weni_sale_sector = fields.Selection(
+        string='Sector',
+        selection=[
+            ('un', 'UN'),
+            ('private', 'Private Market'),
+            ('social', 'Social Impact'),
+            ('government', 'Government')
+        ],)
+    weni_linkedin_profile = fields.Char(
+        string="Linkedin Profile"
+    )
+    weni_lost_date = fields.Date(
+        'Lost Date',
+        track_visibility='always'
+    )
+    weni_is_freezed = fields.Boolean(
+        string='Is freezed'
+    )
+
+    # ICP Details
+    weni_company_size = fields.Integer(
+        string='Company Size'
+    )
+    weni_active_contacts = fields.Integer(
+        string='Active Contacts'
+    )
+    weni_has_dedicate_team = fields.Boolean(
+        string="Has Dedicated Team"
+    )
+    weni_customer_rating = fields.Float(
+        string="Customer Rating"
+    )
+    weni_total_human_agents = fields.Integer(
+        string="Total Human Agents"
+    )
+    weni_use_chatbot_solution = fields.Boolean(
+        string="Use Chatbot Solution"
+    )
+    weni_systems_used = fields.Html(
+        string='Systems Used'
+    )
     weni_icp_classification = fields.Selection(
         string='ICP Classification',
         selection=[
@@ -47,6 +81,32 @@ class CrmLead(models.Model):
             ('medium_fit', 'Medium Fit'),
             ('high_fit', 'High Fit')
         ],)
+
+
+    # Diagnostic
+    weni_current_situation = fields.Html(
+        string='Current Situation'
+    )
+    weni_project_pain_primary = fields.Html(
+        string='Primary Problems'
+    )
+    weni_project_pain_secondary = fields.Html(
+        string='Secondary Problems'
+    )
+    weni_implication = fields.Html(
+        string='Implication'
+    )
+    weni_need = fields.Html(
+        string='Need'
+    )
+    weni_project_integration = fields.Html(
+        string='Integrations'
+    )
+    weni_channels_used = fields.Html(
+        string='Channels Used'
+    )
+
+    # TODO: Não vão utilizar
     weni_channel_ids = fields.Many2many(
         "crm.lead.channel",
         "crm_leed_channel_rel",
@@ -54,6 +114,8 @@ class CrmLead(models.Model):
         "channel_id",
         "Channels",
     )
+
+    # Decision Makers
     weni_decision_maker_ids = fields.One2many(
         comodel_name='crm.lead.decision.maker',
         inverse_name='lead_id',
@@ -64,9 +126,15 @@ class CrmLead(models.Model):
         inverse_name='lead_id',
         string='Users',
     )
-    weni_sale_type = fields.Selection(
-        string='Sale Type',
-        selection=[
-            ('project', 'Project'),
-            ('platform', 'Platform'),
-        ],)
+
+    @api.multi
+    def action_set_lost(self):
+        super().action_set_lost()
+        return self.write({'weni_lost_date': date.today()})
+
+    def toggle_active(self):
+        res = super(CrmLead, self).toggle_active()
+        for lead in self.filtered(lambda lead: lead.active):
+            lead.lost_reason = False
+            lead.weni_lost_date = False
+        return res
