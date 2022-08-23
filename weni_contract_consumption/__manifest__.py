73eb35803d301864d95d3439bf1990c0bd5e8e0e
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:23:17 -0300

    [IMP] weni_contract_consumption: pre-commit

--- weni_contract_consumption/__manifest__.py
+++ weni_contract_consumption/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Weni Contract Consumption',
-    'description': """
+    "name": "Weni Contract Consumption",
+    "summary": """
         Weni Contract Custom""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://www.escodoo.com.br',
-    'depends': [
-        'sale',
-        'l10n_br_contract',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/weni-addons",
+    "depends": [
+        "sale",
+        "l10n_br_contract",
     ],
-    'data': [
-        'views/contract_contract.xml',
-        'security/contract_line_consumption.xml',
-        'views/contract_line_consumption.xml',
+    "data": [
+        "views/contract_contract.xml",
+        "security/contract_line_consumption.xml",
+        "views/contract_line_consumption.xml",
     ],
 }
--- weni_contract_consumption/models/contract_line_consumption.py
+++ weni_contract_consumption/models/contract_line_consumption.py
@@ -1,55 +1,56 @@
 # Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 
 
 class ContractLineConsumption(models.Model):
 
-    _name = 'contract.line.consumption'
-    _description = 'Contract Line Consumption'
-    _rec_name = 'contract_line_id'
+    _name = "contract.line.consumption"
+    _description = "Contract Line Consumption"
+    _rec_name = "contract_line_id"
 
     contract_id = fields.Many2one(
-        comodel_name='contract.contract',
+        comodel_name="contract.contract",
         index=True,
-        required='True',
+        required="True",
     )
 
     partner_id = fields.Many2one(
-        related='contract_id.partner_id',
-        string='Partner',
-        store='True',
+        related="contract_id.partner_id",
+        string="Partner",
+        store="True",
     )
 
     contract_line_id = fields.Many2one(
-        comodel_name='contract.line',
+        comodel_name="contract.line",
         index=True,
-        required='True',
+        required="True",
     )
 
     product_id = fields.Many2one(
-        related='contract_line_id.product_id',
-        string='Product',
-        store = 'True',
+        related="contract_line_id.product_id",
+        string="Product",
+        store="True",
     )
 
     consumption_date = fields.Date(
-        string='Consumption Date',
-        index=True,
-        required='True'
+        string="Consumption Date", index=True, required="True"
     )
 
-    consumption_quantity = fields.Integer(
-        string='Consumption Quantity')
+    consumption_quantity = fields.Integer(string="Consumption Quantity")
 
-    invoice_status = fields.Selection([
-        ('invoiced', 'Fully Invoiced'),
-        ('to_be_invoice', 'To Invoice'),
-    ], string='Invoice Status', readonly=True, default='to_be_invoice'
+    invoice_status = fields.Selection(
+        [
+            ("invoiced", "Fully Invoiced"),
+            ("to_be_invoice", "To Invoice"),
+        ],
+        string="Invoice Status",
+        readonly=True,
+        default="to_be_invoice",
     )
 
-    @api.onchange('contract_id')
+    @api.onchange("contract_id")
     def _onchange_contract_id(self):
         for rec in self:
             rec.contract_line_id = False
