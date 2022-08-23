debdaaf85e4148e2db671f065f41b40d949e9cc3
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:26:35 -0300

    [IMP] weni_product_contract_custom: pre-commit

--- weni_product_contract_custom/__manifest__.py
+++ weni_product_contract_custom/__manifest__.py
@@ -2,14 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Weni Product Contract Custom',
-    'description': """
+    "name": "Weni Product Contract Custom",
+    "summary": """
         Weni Product Contract Custom""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://www.escodoo.com.br',
-    'depends': [
-        'product_contract'
-    ],
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/weni-addons",
+    "depends": ["product_contract"],
 }
--- weni_product_contract_custom/models/sale_order_line.py
+++ weni_product_contract_custom/models/sale_order_line.py
@@ -1,13 +1,14 @@
 # Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
 from dateutil.relativedelta import relativedelta
+
+from odoo import models
 
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _get_date_end(self):
         # Originally the suggested end date is multiplied by the number
@@ -15,16 +16,18 @@ class SaleOrderLine(models.Model):
 
         self.ensure_one()
 
-        # It is necessary to completely overwrite this method because when selling a large amount of the product, the
-        # system returns an error when calculating the original end date since there was an overflow of the allowed
-        # range of years.
+        # It is necessary to completely overwrite this method because when
+        # selling a large amount of the product, the system returns an error
+        # when calculating the original end date since there was an overflow
+        # of the allowed range of years.
         # super()._get_date_end()
 
         contract_line_model = self.env["contract.line"]
         date_end = (
             self.date_start
             + contract_line_model.get_relative_delta(
-                self._get_auto_renew_rule_type(), 12,
+                self._get_auto_renew_rule_type(),
+                12,
             )
             - relativedelta(days=1)
         )
