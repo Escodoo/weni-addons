105a66401c0db60b24639ff0638a391d6144f1e5
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:22:08 -0300

    [IMP] weni_account_custom: pre-commit

--- weni_account_custom/__manifest__.py
+++ weni_account_custom/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Weni Account Custom',
-    'description': """
+    "name": "Weni Account Custom",
+    "summary": """
         WENI Account Custom""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://www.escodoo.com.br',
-    'depends': [
-        'account',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/weni-addons",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/account_invoice.xml',
+    "data": [
+        "views/account_invoice.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
