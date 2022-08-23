11ba4301d0a90fa05ba4c89f96b522049acd22fc
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:24:06 -0300

    [IMP] weni_contract_mis_builder_cash_flow: pre-commit

--- weni_contract_mis_builder_cash_flow/__manifest__.py
+++ weni_contract_mis_builder_cash_flow/__manifest__.py
@@ -2,23 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Weni Contract Mis Builder Cash Flow',
-    'summary': """
+    "name": "Weni Contract Mis Builder Cash Flow",
+    "summary": """
         Weni Contract Mis Builder Cash Flow""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://www.escodoo.com.br',
-    'depends': [
-        'weni_mis_builder_cash_flow_custom',
-        'contract',
-        'queue_job',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/weni-addons",
+    "depends": [
+        "weni_mis_builder_cash_flow_custom",
+        "contract",
+        "queue_job",
     ],
-    'data': [
-        'views/res_config_settings.xml',
-        'views/res_company.xml',
-        'data/contract_forecast_line_cron.xml',
+    "data": [
+        "views/res_config_settings.xml",
+        "views/res_company.xml",
+        "data/contract_forecast_line_cron.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
