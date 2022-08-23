bde58311c35278ffae2606fc2be5c2ee863b9758
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:25:40 -0300

    [IMP] weni_partner_nps: pre-commit

--- weni_partner_nps/models/res_partner.py
+++ weni_partner_nps/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -19,29 +19,33 @@ class ResPartner(models.Model):
         for rec in self:
             if rec.weni_nps_ids:
                 if rec.weni_current_nps <= 3:
-                    rec.weni_current_nps_classification = 'danger'
+                    rec.weni_current_nps_classification = "danger"
                 elif rec.weni_current_nps > 3 and rec.weni_current_nps <= 6:
-                    rec.weni_current_nps_classification = 'alert'
+                    rec.weni_current_nps_classification = "alert"
                 elif rec.weni_current_nps > 6:
-                    rec.weni_current_nps_classification = 'success'
+                    rec.weni_current_nps_classification = "success"
                 else:
                     rec.weni_current_nps_classification = False
             else:
                 rec.weni_current_nps_classification = False
 
-    weni_current_nps = fields.Integer(compute='_compute_partner_nps', string=' NPS', store=True)
-    weni_nps_ids = fields.One2many('res.partner.nps', 'partner_id', 'NPSs')
+    weni_current_nps = fields.Integer(
+        compute="_compute_partner_nps", string=" NPS", store=True
+    )
+    weni_nps_ids = fields.One2many("res.partner.nps", "partner_id", "NPSs")
     weni_nps_count = fields.Integer(
-        compute=_compute_weni_nps_count, string="Number of NPSs",
+        compute=_compute_weni_nps_count,
+        string="Number of NPSs",
     )
 
-    @api.depends('weni_nps_ids')
+    @api.depends("weni_nps_ids")
     def _compute_partner_nps(self):
         for rec in self:
-            nps_record = self.env['res.partner.nps'].search([
-                ('partner_id', '=', rec.id),
-                ('return_date', '!=', False)
-            ], order='submission_date desc, nps asc', limit=1)
+            nps_record = self.env["res.partner.nps"].search(
+                [("partner_id", "=", rec.id), ("return_date", "!=", False)],
+                order="submission_date desc, nps asc",
+                limit=1,
+            )
 
             rec.weni_current_nps = nps_record.nps
             # # TODO: Precisa alinhar com o cliente se isto de fato é necessário
@@ -52,6 +56,8 @@ class ResPartner(models.Model):
         if self.weni_nps_count > 1:
             action["domain"] = [("id", "in", self.weni_nps_ids.ids)]
         else:
-            action["views"] = [(self.env.ref("weni_partner_nps.res_partner_nps_form_view").id, "form")]
+            action["views"] = [
+                (self.env.ref("weni_partner_nps.res_partner_nps_form_view").id, "form")
+            ]
             action["res_id"] = self.weni_nps_ids and self.weni_nps_ids.ids[0] or False
         return action
