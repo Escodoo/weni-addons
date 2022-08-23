3e70d70361fe4a7860cfb145c7c0c997974ae768
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:27:40 -0300

    [IMP] weni_sale_custom: pre-commit

--- weni_sale_custom/models/sale_order.py
+++ weni_sale_custom/models/sale_order.py
@@ -1,9 +1,9 @@
 # Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import models
 
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
