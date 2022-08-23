54fd9a2594cdae33eb702b90a9a38062f9e5981f
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:24:44 -0300

    [IMP] weni_crm_custom: pre-commit

--- weni_crm_custom/__manifest__.py
+++ weni_crm_custom/__manifest__.py
@@ -2,27 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Weni Crm Custom',
-    'description': """
+    "name": "Weni Crm Custom",
+    "summary": """
         Customizações do Modulo CRM - Weni""",
-    'version': '12.0.2.0.0',
-    'license': 'AGPL-3',
-    'author': 'Escodoo',
-    'website': 'https://www.escodoo.com.br',
-    'depends': [
-        'l10n_br_crm',
-        'crm_secondary_salesperson',
-        'crm_industry',
+    "version": "12.0.2.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/weni-addons",
+    "depends": [
+        "l10n_br_crm",
+        "crm_secondary_salesperson",
+        "crm_industry",
     ],
-    'data': [
-        'security/crm_lead_user.xml',
-        'views/crm_lead_user.xml',
-        'security/crm_lead_decision_maker.xml',
-        'views/crm_lead_decision_maker.xml',
-        'security/crm_lead_channel.xml',
-        'views/crm_lead_channel.xml',
-        'views/crm_lead.xml',
+    "data": [
+        "security/crm_lead_user.xml",
+        "views/crm_lead_user.xml",
+        "security/crm_lead_decision_maker.xml",
+        "views/crm_lead_decision_maker.xml",
+        "security/crm_lead_channel.xml",
+        "views/crm_lead_channel.xml",
+        "views/crm_lead.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
--- weni_crm_custom/models/crm_lead_user.py
+++ weni_crm_custom/models/crm_lead_user.py
@@ -1,26 +1,18 @@
 # Copyright 2021 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class CrmLeadUser(models.Model):
 
-    _name = 'crm.lead.user'
-    _description = 'Crm Lead User'  # TODO
+    _name = "crm.lead.user"
+    _description = "Crm Lead User"  # TODO
 
     name = fields.Char()
-    lead_id = fields.Many2one(
-        'crm.lead',
-        string='Lead')
-    user_id = fields.Many2one(
-        'res.partner',
-        string='User')
-    function = fields.Char(
-        related='user_id.function',
-        string='Job Position')
-    email = fields.Char(
-        related='user_id.email',
-        readonly=True)
-    phone = fields.Char(related='user_id.phone')
-    mobile = fields.Char(related='user_id.mobile')
+    lead_id = fields.Many2one("crm.lead", string="Lead")
+    user_id = fields.Many2one("res.partner", string="User")
+    function = fields.Char(related="user_id.function", string="Job Position")
+    email = fields.Char(related="user_id.email", readonly=True)
+    phone = fields.Char(related="user_id.phone")
+    mobile = fields.Char(related="user_id.mobile")
