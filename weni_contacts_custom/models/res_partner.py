c62243272a9b912b92383e5be55be68aad8e8141
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-08-23 11:22:35 -0300

    [IMP] weni_contacts_custom: pre-commit

--- weni_contacts_custom/models/res_partner.py
+++ weni_contacts_custom/models/res_partner.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from datetime import date
-from odoo import _, api, fields, models
+
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -10,84 +11,85 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     weni_customer_size = fields.Selection(
-        string='Customer Size',
-        selection=([
-            ('micro','Micro'),
-            ('small','Small'),
-            ('medium','Medium'),
-            ('large','Large')
-        ])
+        string="Customer Size",
+        selection=(
+            [
+                ("micro", "Micro"),
+                ("small", "Small"),
+                ("medium", "Medium"),
+                ("large", "Large"),
+            ]
+        ),
     )
 
     weni_service_type_id = fields.Many2one(
-        comodel_name='weni.customer.service.type',
+        comodel_name="weni.customer.service.type",
         string="Type of Service",
     )
 
     weni_cs_analyst_id = fields.Many2one(
-        comodel_name='res.users',
-        string='CS Analyst',
+        comodel_name="res.users",
+        string="CS Analyst",
     )
 
     weni_service_analyst_id = fields.Many2one(
-        comodel_name='res.users',
-        string='Service Analyst',
+        comodel_name="res.users",
+        string="Service Analyst",
     )
 
-    weni_customer_at_risk = fields.Boolean(
-        string='Customer at risk'
-    )
+    weni_customer_at_risk = fields.Boolean(string="Customer at risk")
 
     weni_customer_status_id = fields.Many2one(
-        comodel_name='weni.customer.status',
-        string='Customer Status',
+        comodel_name="weni.customer.status",
+        string="Customer Status",
     )
 
     weni_customer_mrr_scale_id = fields.Many2one(
-        comodel_name='weni.customer.mrr.scale',
-        string="MRR Scale"
+        comodel_name="weni.customer.mrr.scale", string="MRR Scale"
     )
 
-    weni_churn_request_date = fields.Date(
-        string='Churn request date'
-    )
+    weni_churn_request_date = fields.Date(string="Churn request date")
 
     weni_months_customer_lifetime = fields.Integer(
-        string='Months of customer lifetime',
-        compute='_compute_months_customer_lifetime',
-        store=True
+        string="Months of customer lifetime",
+        compute="_compute_months_customer_lifetime",
+        store=True,
     )
 
-    weni_has_dedicate_team = fields.Boolean(
-        string="Has Dedicated Team"
-    )
+    weni_has_dedicate_team = fields.Boolean(string="Has Dedicated Team")
 
     @api.multi
-    @api.depends('commercial_partner_id',
-                 'commercial_partner_id.child_ids',
-                 'commercial_partner_id.invoice_ids',
-                 'commercial_partner_id.invoice_ids.state',
-                 'commercial_partner_id.child_ids.invoice_ids',
-                 'commercial_partner_id.child_ids.invoice_ids.state')
+    @api.depends(
+        "commercial_partner_id",
+        "commercial_partner_id.child_ids",
+        "commercial_partner_id.invoice_ids",
+        "commercial_partner_id.invoice_ids.state",
+        "commercial_partner_id.child_ids.invoice_ids",
+        "commercial_partner_id.child_ids.invoice_ids.state",
+    )
     def _compute_months_customer_lifetime(self):
         for rec in self:
             num_months = 0
-            if rec.company_type == 'company' and rec.customer:
+            if rec.company_type == "company" and rec.customer:
                 invoice_date = rec.sudo().search_date_first_invoice()
                 if invoice_date and invoice_date < date.today():
                     end_date = date.today()
                     start_date = invoice_date
-                    num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
+                    num_months = (end_date.year - start_date.year) * 12 + (
+                        end_date.month - start_date.month
+                    )
             rec.weni_months_customer_lifetime = num_months
 
     def search_date_first_invoice(self):
         invoice_date = False
         invoice_ids = (
-                self.commercial_partner_id.invoice_ids +
-                self.commercial_partner_id.mapped(
-                    'child_ids.invoice_ids'))
+            self.commercial_partner_id.invoice_ids
+            + self.commercial_partner_id.mapped("child_ids.invoice_ids")
+        )
         if len(invoice_ids) > 0:
-            invoice_ids = invoice_ids.filtered(lambda x: x.state == 'open' and x.type == 'out_invoice')
+            invoice_ids = invoice_ids.filtered(
+                lambda x: x.state == "open" and x.type == "out_invoice"
+            )
             if len(invoice_ids) > 0:
                 invoice_id = invoice_ids.sorted(key=lambda r: r.date_invoice)[0]
                 invoice_date = invoice_id.date_invoice
