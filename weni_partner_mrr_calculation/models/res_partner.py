9cf001a40009be51689750394ceb595f28ecbd48
Eduardo Lima <eduardolimrib@gmail.com>
2022-08-31 18:56:04 -0300

    [IMP] Improved tests with invoice models

--- weni_partner_mrr_calculation/models/res_partner.py
+++ weni_partner_mrr_calculation/models/res_partner.py
@@ -10,6 +10,15 @@ class ResPartner(models.Model):
 
     _inherit = "res.partner"
 
+    def search_max(self, invoice_id):
+        return self.env["weni.customer.mrr.scale"].search(
+            [
+                ("min_value", "<=", invoice_id.amount_total_company_signed),
+                ("max_value", ">=", invoice_id.amount_total_company_signed),
+            ],
+            limit=1,
+        )
+
     @api.model
     def _cron_update_mrr(self):
         partners = self.env["res.partner"].search(
@@ -30,11 +39,5 @@ class ResPartner(models.Model):
                 )
                 if len(invoice_ids) > 0:
                     invoice_id = invoice_ids.sorted(key=lambda r: r.date_invoice)[-1]
-                    mrr = self.env["weni.customer.mrr.scale"].search(
-                        [
-                            ("min_value", "<=", invoice_id.amount_total_company_signed),
-                            ("max_value", ">=", invoice_id.amount_total_company_signed),
-                        ],
-                        limit=1,
-                    )
+                    mrr = self.search_max(invoice_id)
             partner.weni_customer_mrr_scale_id = mrr
--- weni_partner_mrr_calculation/tests/test_customer_mrr_scale.py
+++ weni_partner_mrr_calculation/tests/test_customer_mrr_scale.py
@@ -6,41 +6,98 @@ class TestMrrScale(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         mrr_scale = cls.env["weni.customer.mrr.scale"]
+        cls.currency_id = cls.env.ref("base.USD").id
+        cls.user_admin = cls.env.ref("base.user_root")
+        cls.user_demo = cls.env.ref("base.user_demo")
+        cls.user_portal = cls.env.ref("base.demo_user0")
 
         cls.mrr = mrr_scale.create(
             {
                 "name": "Test Partner",
-                "company_id": cls.env.user.company_id.id,
+                "currency_id": 1,
                 "min_value": 50.00,
                 "max_value": 1000.00,
             }
         )
-        cls.amount = 0
+        cls.account_type = cls.env["account.account.type"].create(
+            {
+                "name": "Test",
+                "type": "receivable",
+            }
+        )
+        cls.account = cls.env["account.account"].create(
+            {
+                "name": "Test account",
+                "code": "TEST",
+                "user_type_id": cls.account_type.id,
+                "reconcile": True,
+            }
+        )
+        cls.partner_1 = cls.env["res.partner"].create(
+            {
+                "name": "Mr. Odoo",
+            }
+        )
+        cls.partner_2 = cls.env["res.partner"].create(
+            {
+                "name": "Mrs. Odoo",
+            }
+        )
+        cls.tax = cls.env["account.tax"].create(
+            {
+                "name": "NO TAX",
+                "amount_type": "percent",
+                "type_tax_use": "purchase",
+                "amount": 0,
+            }
+        )
+        cls.journal = cls.env["account.journal"].create(
+            {
+                "name": "Test purchase journal",
+                "code": "TPUR",
+                "type": "purchase",
+            }
+        )
+        cls.invoice = cls.env["account.invoice"].create(
+            {
+                "name": "Test Customer Invoice",
+                "journal_id": cls.journal.id,
+                "partner_id": cls.partner_1.id,
+                "account_id": cls.account.id,
+                "type": "in_invoice",
+            }
+        )
+        cls.invoice_line = cls.env["account.invoice.line"]
+        cls.invoice_line1 = cls.invoice_line.create(
+            {
+                "invoice_id": cls.invoice.id,
+                "name": "Line 1",
+                "price_unit": 200.00,
+                "account_id": cls.account.id,
+                "invoice_line_tax_ids": [(6, 0, [cls.tax.id])],
+                "quantity": 1,
+            }
+        )
+        cls.invoice._onchange_invoice_line_ids()
+
+    def test_mrr_name(self):
+        self.assertTrue(self.mrr.name, "MRR should have a name.")
 
     def test_mrr_limits(self):
-        self.amount = 700.00
+        self.assertEqual(self.invoice.amount_total, 200.00)
         self.assertTrue(
-            self.mrr.search(
-                [("min_value", "<=", self.amount), ("max_value", ">=", self.amount)],
-                limit=1,
-            ),
+            True if self.partner_1.search_max(self.invoice) else False,
             "MRR: Should return true for value between limits.",
         )
-
-        self.amount = 1200.00
+        self.invoice_line1.price_unit = 1200
+        self.invoice._onchange_invoice_line_ids()
         self.assertFalse(
-            self.mrr.search(
-                [("min_value", "<=", self.amount), ("max_value", ">=", self.amount)],
-                limit=1,
-            ),
+            True if self.partner_1.search_max(self.invoice) else False,
             "MRR: Should return false for value above upper limit.",
         )
-
-        self.amount = 40.00
+        self.invoice_line1.price_unit = 40
+        self.invoice._onchange_invoice_line_ids()
         self.assertFalse(
-            self.mrr.search(
-                [("min_value", "<=", self.amount), ("max_value", ">=", self.amount)],
-                limit=1,
-            ),
+            True if self.partner_1.search_max(self.invoice) else False,
             "MRR: Should return true for value below lower limit.",
         )
