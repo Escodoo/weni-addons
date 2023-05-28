97c5785d889a2e611fee0b00c544ccaab922633b
Kaynnan Lemes <kaynnanx1@gmail.com>
2023-05-28 18:37:52 -0300

    [IMP] weni_project_custom:
        - Refactored project.task and removed weni_kanban_state_fields
        - Created a custom widget extending StateSelection
        - Replaced kanban_state field in project.task view to use the custom widget

--- weni_project_custom/__manifest__.py
+++ weni_project_custom/__manifest__.py
@@ -5,17 +5,21 @@
     "name": "Weni Project Custom",
     "summary": """
         Weni Project Custom""",
-    "version": "12.0.2.0.0",
+    "version": "12.0.3.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/weni-addons",
     "depends": ["project", "web_widget_color"],
     "data": [
         "security/project_task_kanban_state.xml",
+        "views/assets.xml",
         "views/project_task_kanban_state.xml",
         "views/project_task.xml",
         "views/project_project.xml",
         "views/res_partner.xml",
+    ],
+    "qweb": [
+        "static/src/xml/base.xml",
     ],
     "demo": [],
 }
--- weni_project_custom/models/project_task_kanban_state.py
+++ weni_project_custom/models/project_task_kanban_state.py
@@ -1,7 +1,7 @@
 # Copyright 2023 - TODAY, Kaynnan Lemes <kaynnan.lemes@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProjectTaskKanbanState(models.Model):
@@ -10,3 +10,17 @@ class ProjectTaskKanbanState(models.Model):
 
     name = fields.Char(string="Name", required=True)
     color = fields.Char(string="Color", required=True, help="Choose a color")
+
+    @api.model
+    def search_records_by_name(self, selection_item):
+        domain = [("name", "=", selection_item)]
+        records = self.search(domain)
+        result = []
+        for record in records:
+            result.append(
+                {
+                    "name": record.name,
+                    "color": record.color,
+                }
+            )
+        return result
